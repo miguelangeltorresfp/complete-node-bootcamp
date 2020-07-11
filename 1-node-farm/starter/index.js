dce35e3768b0d9899b5462896ea1eef6043e0389
Miguel Angel Torres FP <miguelangeltorresfp@gmail.com>
2020-07-11 17:28:57 +0200

    1_Reading_Files

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log('textIn: ', textIn);
+
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written!');
+
+// Non-blocking, asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  if (err) return console.log('ERROR 💥');
+
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+      console.log(data3);
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+        console.log('Your file has been written 😊');
+      });
+    });
+  });
+});
+console.log('Will read file!');

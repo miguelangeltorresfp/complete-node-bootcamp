e47b1a6baa79c2b1f266f0d3e7d775094a7019ae
Miguel Angel Torres FP <miguelangeltorresfp@gmail.com>
2020-07-11 19:15:09 +0200

    Creating_a_simple_web_server

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
+const http = require('http');
+
+//////////////////////////////
+// FILES
 
 // Blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -9,17 +13,27 @@ const fs = require('fs');
 // console.log('File written!');
 
 // Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  if (err) return console.log('ERROR 💥');
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('ERROR 💥');
 
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-      console.log(data3);
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
-        console.log('Your file has been written 😊');
-      });
-    });
-  });
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+//         console.log('Your file has been written 😊');
+//       });
+//     });
+//   });
+// });
+// console.log('Will read file!');
+
+////////////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server');
 });
-console.log('Will read file!');
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listen te request on port 8000');
+});
